b3ddaf22160979854910c09c41e14dc44f518e28
dssilva05 <dssilva.05@gmail.com>
2026-02-03 11:16:44 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'diario-sobral-v1';
+const CACHE_NAME = 'sobral-diario-v2';
 const assets = [
   './',
   './index.html',
   'https://images.unsplash.com/photo-1497633762265-9d179a990aa6?q=80'
 ];
 
-// Instala o service worker e guarda os arquivos no cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -14,11 +13,10 @@ self.addEventListener('install', event => {
   );
 });
 
-// Responde mesmo quando estiver offline
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
